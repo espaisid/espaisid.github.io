ffe8256115d9913b0817d8a7a59888b505973cf8
freecates <ramongil@ramongil.cat>
2015-02-06 12:37:53 +0100

    Nova versió del web via service worker.

---
+++ worker.js
@@ -0,0 +1,112 @@
+---
+---
+importScripts('/bower_components/cache-polyfill/dist/serviceworker-cache-polyfill.js');
+
+// The SW will be shutdown when not in use to save memory,
+// be aware that any global state is likely to disappear
+
+var DEFAULT_CACHE = 'espaisid-app-cache0';
+var AIP_CACHE = 'espaisid-aip-cache0';
+
+console.log('SW startup', DEFAULT_CACHE);
+
+self.addEventListener('install', function(event) {
+  // pre cache a load of stuff:
+  event.waitUntil(
+    caches.open(DEFAULT_CACHE).then(function(cache) {
+      return cache.addAll([
+        '/styles/main.css',
+        '/font-awesome/css/font-awesome.min.css',
+        '/font-artinpocket/dist/stylesheet.min.css',
+        '/feed.xml',
+        '/images/touch/chrome-touch-icon-192x192.png',
+        '/apple-touch-icon-precomposed.png',
+        '/images/touch/ms-touch-icon-144x144-precomposed.png',
+        '/favicon.ico',
+        '/images/artinpocket-organitzadors-barcelona-digital-art-footer.jpg',
+        '/images/oxygen-organitzadors-barcelona-digital-art-footer.jpg',
+        '/images/sid-publicitat-organitzadors-espaisid.jpg',
+        '/images/facana-espai-sid.jpg',
+        '/images/orange-de-angels-estella-acuarela-grayscale-espaisid.jpg',
+        '/images/acuarela-angels-estella-walk-grayscale-espaisid.jpg',
+        '/bower_components/google-map/google-map.min.html',
+        '/bower_components/core-image/core-image.min.html',
+        '/bower_components/google-apis/google-apis.min.html',
+        '/bower_components/polymer/polymer.html',
+        '/bower_components/polymer/layout.min.html',
+        '/bower_components/polymer/polymer.min.js',
+        '/bower_components/webcomponentsjs/webcomponents.min.js',
+        '/scripts/jquery-1.11.0.min.js',
+        '/scripts/bootstrap.min.js',
+        '/scripts/jquery.easing.min.js',
+        '/scripts/grayscale.min.js',
+        '/vendor/add-to-homescreen/addtohomescreen.min.js',
+        '/vendor/add-to-homescreen/addtohomescreen.min.css',
+        {% for page in site.pages %}{% if page.url != '/worker.js' and page.url != '/404.html' %}'{{ page.url }}',{% endif %}
+        {% endfor %}{% for post in site.posts %}{% if post.url != '/worker.js' and post.url != '/404.html' and forloop.last != true %}'{{ post.url }}',{% elsif post.url != '/worker.js' and post.url != '/404.html' and forloop.last == true %}'{{ post.url }}'{% endif %}
+        {% endfor %}
+      ]);
+    })
+  )
+});
+
+// Remove any cache that isn't on the whitelist
+// This is a good way to clean up left over caches
+// when a new Service Worker is installed
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [DEFAULT_CACHE, AIP_CACHE];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// During a fetch, check to see if the requested
+// resource is already cached, if so, return it.
+// If the requested resource comes from Amazon S3,
+// dynamically cache it.
+self.addEventListener('fetch', function(event) {
+  console.log('fetching', event.request.url);
+  var requestURL = new URL(event.request.url);
+
+  if (requestURL.hostname == 'maps.googleapis.com') {
+    console.log('fetching from aip');
+    event.respondWith(aipResponse(event.request));
+  } else {
+    console.log('fetching generic asset');
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
+
+function aipResponse(request) {
+  return caches.match(request).then(function(response) {
+    if (response) {
+      console.log('found match in aip cache');
+      return response;
+    }
+
+    return fetch(request).then(function(response) {
+      caches.open(AIP_CACHE).then(function(cache) {
+        cache.put(request, response).then(function() {
+          console.log('yey img cache');
+        }, function() {
+          console.log('nay img cache');
+        });
+      });
+
+      return response.clone();
+    });
+  });
+}
